da8d4e23c38dc1bf926aa1ee231e3a57a87e09b7
nxveeen <nilotpalgoyal9393@gmail.com>
2021-09-02 19:27:24 +0530

    desktop layout added

--- dist/main.2026268b.js
+++ dist/main.2026268b.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"C:\\Users\\Naveen\\Desktop\\myapp\\1. Sunnyside\\src\\images\\mobile\\image-header.jpg":[["image-header.348e0754.jpg","src/images/mobile/image-header.jpg"],"src/images/mobile/image-header.jpg"],"C:\\Users\\Naveen\\Desktop\\myapp\\1. Sunnyside\\src\\images\\desktop\\image-header.jpg":[["image-header.51e12821.jpg","src/images/desktop/image-header.jpg"],"src/images/desktop/image-header.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53639" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61975" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
